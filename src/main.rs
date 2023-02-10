58bdf355fd13562699d0b5f67463418ae3fae77a
Joseph Heyes <josephheyes@gmail.com>
2023-02-10 16:54:41 +0000

    Add structs and chrono crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use ical;
+use chrono::{DateTime, Local};
+use std::io::BufReader;
+use std::fs::File;
+
+pub struct Calendar {
+    pub events: Vec<Event>
+}
+
+pub struct Event {
+    pub title: String,
+    pub desc: String,
+    pub t_start: DateTime<Local>,
+    pub t_end: DateTime<Local>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let buf = BufReader::new(File::open("/tmp/calendar.ics").unwrap());
+
+    let reader = ical::IcalParser::new(buf);
+
+    
 }
